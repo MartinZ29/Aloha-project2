19be707cb479d4c7eafb9fdf1cb60d080260646e
martinz <martinzhangyx@gmail.com>
2016-12-01 00:16:39 -0800

    smooth scrolling

--- js/js.js
+++ js/js.js
@@ -12,3 +12,16 @@ $('.gallery').flickity({
 $('.sign-up-email').submit( function(){
     return false;
 })
+
+$('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
+});
